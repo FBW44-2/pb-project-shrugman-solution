284b22d0505a527ad9b1706332c2a6816bbce22f
Biliana <bilianavaleva@gmail.com>
2021-03-08 09:19:34 +0100

    Add more movies and books

--- Shrugman.js
+++ Shrugman.js
@@ -13,10 +13,23 @@ class Shrugman {
                 'Titanic',
                 'Saving Private Ryan',
                 'Braveheart',
+                'The Blind Side',
+                'Enola Holmes',
+                'Love Actually',
+                'Onward',
+                'The Invisible Man',
+                'The Night Clerk'
             ],
             books: [
+                'All Quiet on the Western Front',
+                'Pride and Prejudice',
+                'Arch of Triumph: A Novel of a Man Without a Country',
+                'A Time to Love and a Time to Die',
+                'Three Comrades',
+                'My Name Is Red',
                 'The Neverending Story',
                 'Eleven Minutes',
+                'A Strangeness in My Mind',
                 'The Catcher in the Rye',
                 'Harry Potter and the Goblet of Fire'
             ]
