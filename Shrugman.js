b831ee1fa54ac5660f5bdceefee8a29268180a6d
Biliana <bilianavaleva@gmail.com>
2021-03-08 11:20:55 +0100

    Add more books and movies

--- Shrugman.js
+++ Shrugman.js
@@ -21,11 +21,20 @@ class Shrugman {
                 'Love Actually',
                 'Onward',
                 'The Invisible Man',
-                'The Night Clerk'
+                'The Night Clerk',
+                'Vanguard'
             ],
             books: [
+                'Death In The Clouds',
+                'Sparkling Cyanide',
+                'The Body in the Library',
+                'The Rose and the Yew Tree',
+                'Murder in Mesopotamia',
+                'Murder on the Orient Express',
                 'All Quiet on the Western Front',
                 'Pride and Prejudice',
+                'And Then There Were None',
+                'The ABC Murders',
                 'Arch of Triumph: A Novel of a Man Without a Country',
                 'A Time to Love and a Time to Die',
                 'Three Comrades',
